4b31f481ddf4b2a5ef04760b8ed9630f705a4cc0
ZanissNguyen <phat.nguyenthanh2408@gmail.com>
2025-10-09 07:42:14 +0700

    more init

--- solver.py
+++ solver.py
@@ -8,6 +8,10 @@ pygame.init()
 # boxes - list of (x,y) positions
 # player - (x,y) position
 # goal - list of (x,y) positions
+result = { # return value for threading function
+    "BrFS": {},
+    "Astar": {}
+}
 map = []
 player = (0, 0)
 boxes = set()
@@ -59,13 +63,29 @@ def BrFS(map, start, goal):
     # if no path, return None
 
     # return flag, path, explored_nodes, time_taken, memory_used
-    return True, True, [], 0, 0, 0
+    result['BrFS'] = {
+        'is_solved': False,
+        'is_complete': False, 
+        'path': [], 
+        'explored_node': 0,
+        'time_taken': 0,
+        'memory_used': 0
+    }
+    pass
 
 def A_star(map, start, goal):
     # implement A* algorithm
 
     # return solved flag, path, explored_nodes, time_taken, memory_used
-    return True, True, [], 0, 0, 0
+    result['Astar'] = {
+        'is_solved': False,
+        'is_complete': False, 
+        'path': [], 
+        'explored_node': 0,
+        'time_taken': 0,
+        'memory_used': 0
+    }
+    pass
 
 def A_star_h(map, start, goal):
     # calculate h(n)
@@ -80,6 +100,7 @@ def draw(map, offset_x = 0):
     # display map using pygame 
 
     pygame.display.update()
+    pygame.time.delay(100)
     pass
 
 def replay_path(start_map, path):
@@ -92,46 +113,54 @@ if __name__ == "__main__":
     running = True
     pygame.display.set_caption("Sokoban Solver")
 
+    brfs_draw_done = False
+    astar_draw_done = False
+
     state = game_state[0]
     testcase = 0
     starting_map = []
+    is_solving = False
 
     while running:
 
         # check for game_state
-        if state == "choose_testcase":
+        if state == game_state[0]:
         #     display testcase selection screen
         #     event handle
             pass
-        elif state == "solving":
+        elif state == game_state[1]:
         #     # run BrFS and A* in separate threads
-            brfs_done = False
-            astar_done = False    
-            brfs_path = []
-            astar_path = []
-            brfs_thread = threading.Thread(target=lambda: BrFS(starting_map, player, goal))
-            astar_thread = threading.Thread(target=lambda: A_star(starting_map, player
-            , goal))
-            brfs_thread.start()  
-            astar_thread.start()
-            brfs_thread.join()
-            astar_thread.join()
-            state = game_state[2]
+            if not is_solving:
+                is_solving = True
+                brfs_thread = threading.Thread(target=lambda: BrFS(starting_map, player, goal))
+                astar_thread = threading.Thread(target=lambda: A_star(starting_map, player
+                , goal))
+                brfs_thread.start()  
+                astar_thread.start()
 
+            # result:
+            brfs_done = result['BrFS']['is_complete']
+            brfs_path = result['BrFS']['path']
+
+            astar_done = result['Astar']['is_complete']
+            astar_path = result['Astar']['path']
             # draw
-            if brfs_done:
+            if brfs_done and not brfs_draw_done:
                 # Animate BrFS path step by step
                 brfs_map = replay_path(starting_map, brfs_path)
                 for m in brfs_map:
                     draw(map, 0)
-            if astar_done:
+            if astar_done and not astar_draw_done:
                 # Animate A* path step by step
                 astar_map = replay_path(starting_map, astar_path)
                 for m in astar_map:
                     draw(map, 0) #offset = window_width / 2
-
+            
             # if both completed display button analysis
-        elif state == "solved":
+            # or just go to the next state
+            if brfs_draw_done and astar_draw_done:
+                state = game_state[2]
+        elif state == game_state[2]:
             # display both status: is solve, some evaluation information
             pass
 
